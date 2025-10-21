04620a99c18ce5cd40135ca7b5a8d5bdc217e41c
Venus Poon <venuspsw@hotmail.com>
2025-10-21 11:12:32 +0200

    add if4

--- OtherEx/02If/if.c
+++ OtherEx/02If/if.c
@@ -25,17 +25,40 @@ int main(){
     //     printf("You do not have to order any milk\n");
     // }
 
-    //if 3
-    float temperature = 0.0f;
-    printf("Enter a temperature: ");
-    scanf("%f", &temperature);
-    if (temperature >= 37.8f && temperature < 39.8f){
-        printf("You have fever\n");
-    } else if (temperature >= 39.8f){
-        printf("You should see a doctore\n");
-    } else {
-        printf("You dont have a feber\n");
-    }
+    // //if 3
+    // float temperature = 0.0f;
+    // printf("Enter a temperature: ");
+    // scanf("%f", &temperature);
+    // if (temperature >= 37.8f && temperature < 39.8f){
+    //     printf("You have fever\n");
+    // } else if (temperature >= 39.8f){
+    //     printf("You should see a doctore\n");
+    // } else {
+    //     printf("You dont have a feber\n");
+    // }
 
+    // //if 4
+    // int age = 0.0;
+    // printf("Enter your age: ");
+    // scanf("%d", &age);
+    // if (age < 18){
+    //     printf("Ej myndig\n");
+    // } else if (age >= 65){
+    //     printf("Retire\n");
+    // } else {
+    //     printf("myndig but not retired\n");
+    // }
+
+    //if 5
+    int category = 0;
+    printf("Enter a category: ");
+    scanf("%d", &category);
+    if (category == 2 || category == 3){
+        printf("The ticket costs 20kr\n");
+    } else if (category == 1){
+        printf("The ticket costs 30kr\n");
+    } else {
+        printf("Invalid input");
+    }
     return 0;
 }
