41979cee95f5596e2e066425cbbf06e3ffab403a
Venus Poon <venuspsw@hotmail.com>
2025-10-26 23:02:28 +0100

    add Lopp#1

---
+++ OtherEx/03Loop/Loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+
+    //Loop #1
+    for (int i = 0; i <= 10; i++){
+        printf("%d: %d\n", i, i); 
+    }
+
+    int j = 0;
+    while (j <= 10)
+    {
+        printf("%d: %d\n", j, j); 
+        j++;
+    }
+
+    
+    
+
+    return 0;
+}
