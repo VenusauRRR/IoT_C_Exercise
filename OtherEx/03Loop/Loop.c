fcb41de2b1edda42edc6f16b443e45cc7e6e7c96
Venus Poon <venuspsw@hotmail.com>
2025-10-26 23:23:07 +0100

    add Loop#6 rolling dice

--- OtherEx/03Loop/Loop.c
+++ OtherEx/03Loop/Loop.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <windows.h>
+#include <string.h>
 
 int main(void){
 
-    //Loop #1
-    for (int i = 0; i <= 10; i++){
-        printf("%d: %d\n", i, i); 
-    }
+    // //Loop #1
+    // for (int i = 0; i <= 10; i++){
+    //     printf("%d: %d\n", i, i); 
+    // }
 
-    int j = 0;
-    while (j <= 10)
+    // int j = 0;
+    // while (j <= 10)
+    // {
+    //     printf("%d: %d\n", j, j); 
+    //     j++;
+    // }
+
+    //Loop #6: rolling the dice
+    srand(time(NULL));
+    char answer = '\0';
+    while (true)
     {
-        printf("%d: %d\n", j, j); 
-        j++;
+        printf("\nRolling the dices...\n");
+        printf("The values are...\n");
+        printf("%d\n", rand() % 6 + 1);
+        printf("%d\n", rand() % 6 + 1);
+        printf("Roll the dices again? (Y/N)");
+        scanf(" %c", &answer);
+        if (toupper(answer)=='N'){
+            break;
+        }
     }
-
-    
-    
-
     return 0;
 }
