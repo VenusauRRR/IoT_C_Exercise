0ee57ecfc99d50cd050fe95c9af76bde60e2d565
Venus Poon <venuspsw@hotmail.com>
2025-10-27 08:07:03 +0100

    add main.c to Moms

--- OtherEx/05Function/Moms/main.c
+++ OtherEx/05Function/Moms/main.c
@@ -2,19 +2,19 @@
 
 const float momsPercent = 0.25;
 
-float calculatMoms(totalSum);
+float calculatMoms(float totalSum);
 
 int main(void){
     float totalSum = 0;
 
     printf("Enter the total incl. moms: ");
     scanf("%f", &totalSum);
-    printf("The total is %f and the moms is %f", totalSum, calculatMoms(totalSum));
+    printf("The total is %.2f and the moms is %.2f", totalSum, calculatMoms(totalSum));
 
     return 0;
 }
 
-float calculatMoms(totalSum){
+float calculatMoms(float totalSum){
     return totalSum * momsPercent;
 }
 
