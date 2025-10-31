8ce25a1e6c6efd523ec1dd5dd1cc1065a8863606
Venus Poon <venuspsw@hotmail.com>
2025-11-01 00:03:34 +0100

    add ex12

--- ExtraEx/Array_mellan.c
+++ ExtraEx/Array_mellan.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void ex11_swapPositionInArray(int *index1, int *index2);
+int ex12_counterNumber(int *array, int target, int size);
 
 int main(void){
     // //Exercise 09
@@ -32,19 +33,28 @@ int main(void){
     //     printf("Array sum at index %d: %d\n", i, arrTotal[i]);
     // }
 
-    //Exercise 11
-    // Byt plats på element Låt användaren skriva in 5 tal. Byt plats på första och sista elementet
-    int arr[5] = {0};
-    printf("Enter 5 numbers seperating by space:\n");
-    for(int i = 0; i < 5; i++){
-        scanf("%d", &arr[i]);
-    }
-    ex11_swapPositionInArray(&arr[0], &arr[4]);
-    for (int i = 0; i < 5; i++){
-        printf("number at index %d: %d\n", i, arr[i]);
-    }
+    // //Exercise 11
+    // // Byt plats på element Låt användaren skriva in 5 tal. Byt plats på första och sista elementet
+    // int arr[5] = {0};
+    // printf("Enter 5 numbers seperating by space:\n");
+    // for(int i = 0; i < 5; i++){
+    //     scanf("%d", &arr[i]);
+    // }
+    // ex11_swapPositionInArray(&arr[0], &arr[4]);
+    // for (int i = 0; i < 5; i++){
+    //     printf("number at index %d: %d\n", i, arr[i]);
+    // }
     
-    
+    //Exercise 12
+    //Räkna förekomster Räkna hur många gånger ett visst tal förekommer i arrayen.
+    int arr[10] = {4,9,2,10,3,6,8,2,7,5};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target;
+    printf("Enter a number to count (1-10): ");
+    scanf("%d", &target);
+    int resultCount = ex12_counterNumber(arr, target, size);
+    resultCount == 0 ? printf("No such number\n"): printf("number of count for %d: %d\n", target, resultCount);
+
     return 0;
 }
 
@@ -52,4 +62,16 @@ void ex11_swapPositionInArray(int *index1, int *index2){
     int tmp = *index1;
     *index1 = *index2;
     *index2 = tmp;
+}
+
+int ex12_counterNumber(int *array, int target, int size){
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (array[i] == target){
+            count++;
+        }
+    }
+    return count;
+
 }
