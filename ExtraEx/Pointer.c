ceb097d196074526e6f959e57f6e7ef8ea1e748e
Venus Poon <venuspsw@hotmail.com>
2025-11-04 08:51:10 +0100

    add ex02 pointer and scanf

--- ExtraEx/Pointer.c
+++ ExtraEx/Pointer.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
 
-int main(void){
-    //Ex 01
-//      1. Adress & dereferens Skapa en int x = 42;. Skriv ut värdet, adressen till x, och
-//  värdet via en int *p = &x. Ändra sedan *p = 99; och skriv ut x igen.
-    int x = 42;
+int main(void)
+{
+    // // Ex 01
+    // //      1. Adress & dereferens
+    // // Skapa en int x = 42;. Skriv ut värdet, adressen till x, och
+    // //  värdet via en int *p = &x. Ändra sedan *p = 99; och skriv ut x igen.
+    // int x = 42;
+    // int *p = &x;
+    // printf("x = %d and its address = %p\n", x, &x);
+    // printf("address of p itself is %p\n", &p);
+    // printf("value of p is address of x which is %p, \nthe value of *p is x which is %d\n", p, *p);
+    // *p = 99;
+    // printf("dereference *p has assigned to 99 such that x = %d\n", x);
+
+    // Ex 02
+    //      2. Pekare och scanf
+    //  Läs in ett heltal med scanf("%d", &x); via en pekare p och skriv ut resultatet
+    int x;
     int *p = &x;
-    printf("x = %d and its address = %p\n", x, &x);
-    printf("address of p itself is %p\n", &p);
-    printf("value of p is address of x which is %p, \nthe value of *p is x which is %d\n", p, *p);
-    *p = 99;
-    printf("dereference *p has assigned to 99 such that x = %d\n", x);
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    printf("x = %d\n", *p);
+
 
 
     return 0;
